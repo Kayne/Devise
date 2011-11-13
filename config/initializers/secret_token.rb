cc8116b5c15e909ea5adbddd95cffd435d181d03
Marcin Bratek <github@bratekmarcin.eu>
2011-11-13 19:09:12 +0100

    Create new repository for Devise exercise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devise::Application.config.secret_token = '007b3fcdc032a97fa0a5ec53f1ff0a1a7fdba6a8de046793dd27a4787ab30d1e1294409ae09d4aa463fe230d987c9647653133b079139df74dd1b35cfbe5884a'
